7d4109b595ec4801e05d1292270aed5ec7e0179c
Lahiru Samarakoon <lahiruts@gmail.com>
2015-04-18 11:54:40 +0800

    Mindmap of Research

---
+++ PartitionedDNN.mm
@@ -0,0 +1,39 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1428941909912" ID="ID_370558865" MODIFIED="1429328142630" TEXT="Partitioned DNN">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1429328097084" ID="ID_389942170" MODIFIED="1429328109210" POSITION="right" TEXT="Speaker Representation">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1429328188248" ID="ID_1341056168" MODIFIED="1429328203664" TEXT="i-vectors">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1429328206169" ID="ID_738087237" MODIFIED="1429328221631" TEXT="i-vector cross evaluation">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1429328112677" ID="ID_585940387" MODIFIED="1429328123591" POSITION="left" TEXT="Gain Investigation">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1429328279514" ID="ID_502237559" MODIFIED="1429328318504" TEXT="Multiple runs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1429328320059" ID="ID_1428061521" MODIFIED="1429328413925" TEXT="Add missing weights of Partitioned DNN">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1429328146741" ID="ID_223594294" MODIFIED="1429328257490" POSITION="right" TEXT="Speaker Information Investigation ">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1429328231290" ID="ID_355261629" MODIFIED="1429328245239" TEXT="Add noise to i-vectors">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
